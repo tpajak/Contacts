18a29b01830d2999f02e5606ac8b031ef4348dd7
Tomasz Pająk <tomasz@pajak.name>
2023-05-16 22:53:07 +0200

    Making Contact properties private

--- Contacts (Kotlin)/task/src/Data/Contact.kt
+++
@@ -1,7 +0,0 @@
-package Data
-
-data class Contact(
-    val name:String,
-    val surname:String,
-    val phoneNumber:String,
-)
---
+++ Contacts (Kotlin)/task/src/data/Contact.kt
@@ -0,0 +1,7 @@
+package data
+
+data class Contact(
+    private val name:String,
+    private val surname:String,
+    private val phoneNumber:String,
+)
