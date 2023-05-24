97a90c0b8ea6aed34ed78cf1c116735c9d757000
Tomasz Pająk <tomasz@pajak.name>
2023-05-24 21:16:07 +0200

    expose public Contact GETers

--- Contacts (Kotlin)/task/src/data/Contact.kt
+++ Contacts (Kotlin)/task/src/data/Contact.kt
@@ -1,7 +1,28 @@
 package data
 
 data class Contact(
-    private val name:String,
-    private val surname:String,
-    private val phoneNumber:String,
-)
+    private val _name:String,
+    private val _surname:String,
+    private val _phoneNumber:String,
+) {
+    val name: String
+        get() {
+            return if (_name != "") _name else ""
+        }
+
+    val surname: String
+        get() {
+            return if (_surname != "") _surname else ""
+        }
+
+    val phoneNumber: String
+        get() {
+            return if (_phoneNumber != "") _phoneNumber else "[no number]"
+        }
+
+    override fun toString(): String {
+        return "$name $surname, $phoneNumber"
+    }
+
+
+}
