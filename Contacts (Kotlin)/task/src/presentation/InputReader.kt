f663e0eb6ce87babcd22c3a3ef473490d389882d
Tomasz Pająk <tomasz@pajak.name>
2023-05-24 22:13:00 +0200

    simplify readUseInput

--- Contacts (Kotlin)/task/src/presentation/InputReader.kt
+++ Contacts (Kotlin)/task/src/presentation/InputReader.kt
@@ -3,9 +3,7 @@ package presentation
 class InputReader {
     companion object {
         fun readUserInput(): List<String> {
-            return readLine()?.let {
-                it.split(" ")
-            } ?: "-1" as List<String>
+            return readlnOrNull()?.split(" ") ?: ("-1" as List<String>)
         }
     }
 }
