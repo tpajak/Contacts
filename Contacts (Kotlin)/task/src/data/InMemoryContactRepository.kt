4ec1ef93ff15651bdb95c458c4618af27de1f6cf
Tomasz Pająk <tomasz@pajak.name>
2023-06-26 17:55:11 +0200

    Handling all CRUD operations on Contacts

--- Contacts (Kotlin)/task/src/data/InMemoryContactRepository.kt
+++ Contacts (Kotlin)/task/src/data/InMemoryContactRepository.kt
@@ -1,34 +1,47 @@
 package data
 
+import contacts.data.*
 import contacts.domain.OperationStatus
-import useCase.ContactRepository
+import contacts.useCase.ContactRepository
 
-private val contacts = mutableListOf<Contact>()
 
-class InMemoryContactRepository : ContactRepository {
-    override fun createContact(contact: Contact): Boolean {
-        return contacts.add(contact)
+private val contacts: MutableList<Contact> = mutableListOf()
+
+class InMemoryContactRepository : ContactRepository, IPersonContact, ICompanyContact, IContact {
+    override fun createContact(contact: Contact): OperationStatus {
+        return add(contact)
     }
 
     override fun getContact(id: Int): Contact {
-        TODO("Not yet implemented")
+        return get(id)
     }
 
     override fun updateContactName(id: Int, newName: String): OperationStatus {
-        try {
-            contacts[id - 1].name = newName
-            return OperationStatus.SUCCESS
-        } catch (e: IndexOutOfBoundsException) {
-            return OperationStatus.FAILURE
+        return when (contacts[id - 1]) {
+            is PersonContact -> updatePersonName(id, newName)
+            is CompanyContact -> updateCompanyName(id, newName)
+            else -> OperationStatus.FAILURE
         }
     }
 
     override fun updateContactSurname(id: Int, newSurname: String): OperationStatus {
-        try {
-            contacts[id - 1].surname = newSurname
-            return OperationStatus.SUCCESS
-        } catch (e: IndexOutOfBoundsException) {
-            return OperationStatus.FAILURE
+        return when (contacts[id - 1]) {
+            is PersonContact -> updatePersonSurname(id, newSurname)
+            else -> OperationStatus.FAILURE
+        }
+    }
+
+    override fun updateContactGender(id: Int, newGender: String): OperationStatus {
+        return when (contacts[id - 1]) {
+            is PersonContact -> updatePersonSurname(id, newGender)
+            else -> OperationStatus.FAILURE
+        }
+    }
+
+    override fun updateContactBirth(id: Int, newBirth: String): OperationStatus {
+        return when (contacts[id - 1]) {
+            is PersonContact -> updatePersonSurname(id, newBirth)
+            else -> OperationStatus.FAILURE
         }
     }
 
@@ -41,16 +54,80 @@ class InMemoryContactRepository : ContactRepository {
         }
     }
 
+    override fun updateContactAddress(id: Int, newAddress: String): OperationStatus {
+        return when (contacts[id - 1]) {
+            is CompanyContact -> updateCompanyAddress(id, newAddress)
+            else -> OperationStatus.FAILURE
+        }
+    }
+
     override fun deleteContact(id: Int): OperationStatus {
-        if (countContacts() <= 0) {
-            return OperationStatus.EMPTY_LIST
-        } else {
-            try {
-                contacts.removeAt(id - 1)
-                return OperationStatus.SUCCESS
-            } catch (e: IndexOutOfBoundsException) {
-                return OperationStatus.FAILURE
-            }
+        return delete(id)
+    }
+
+    override fun updateCompanyName(id: Int, newName: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as CompanyContact
+            contact.organisationName = newName
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun updateCompanyAddress(id: Int, newAddress: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as CompanyContact
+            contact.address = newAddress
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun updatePersonName(id: Int, newName: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as PersonContact
+            contact.name = newName
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun updatePersonSurname(id: Int, newSurname: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as PersonContact
+            contact.surname = newSurname
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun updatePersonBirthdate(id: Int, newBirth: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as PersonContact
+            contact.birthdate = newBirth
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun updatePersonGender(id: Int, newGender: String): OperationStatus {
+        return try {
+            val contact = contacts[id - 1]
+            contact as PersonContact
+            contact.gender = newGender
+            OperationStatus.SUCCESS
+        } catch (e: IndexOutOfBoundsException) {
+            OperationStatus.FAILURE
         }
     }
 
@@ -62,5 +139,34 @@ class InMemoryContactRepository : ContactRepository {
         return contacts.count()
     }
 
+    override fun add(contact: Contact): OperationStatus {
+        return if (contact.type == "isPerson" || contact.type == "isCompany") {
+            contacts.add(contact)
+            OperationStatus.SUCCESS
+        } else {
+            OperationStatus.FAILURE
+        }
+    }
+
+    override fun delete(id: Int): OperationStatus {
+        if (countContacts() <= 0) {
+            return OperationStatus.EMPTY_LIST
+        } else {
+            return try {
+                contacts.removeAt(id - 1)
+                OperationStatus.SUCCESS
+            } catch (e: IndexOutOfBoundsException) {
+                OperationStatus.FAILURE
+            }
+        }
+    }
+
+    override fun get(id: Int): Contact {
+        return contacts[id - 1]
+//        return when (contact) {
+//            is PersonContact -> {}
+//            is CompanyContact -> {}
+//        }
+    }
 
 }
