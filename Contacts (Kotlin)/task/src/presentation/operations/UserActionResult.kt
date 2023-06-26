15dc9fc2b84bdf476455b3f03e72feb38a179f88
Tomasz Pająk <tomasz@pajak.name>
2023-06-26 19:27:06 +0200

    cleanup

--- Contacts (Kotlin)/task/src/presentation/operations/UserActionResult.kt
+++ Contacts (Kotlin)/task/src/presentation/operations/UserActionResult.kt
@@ -2,10 +2,4 @@ package operations
 
 sealed class UserActionResult (val command: String) {
     class UserAction(command: String ) : UserActionResult (command = command)
-    class Add(command: String ) : UserActionResult (command = command)
-    class Remove(command: String ) : UserActionResult (command = command)
-    class Edit(command: String ) : UserActionResult (command = command)
-    class Count(command: String ) : UserActionResult (command = command)
-    class List(command: String ) : UserActionResult (command = command)
-    class Exit(command: String ) : UserActionResult (command = command)
 }
